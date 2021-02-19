e635f099d777753d9fd46a6098235d0b40d49de6
haoma <haoma@pptv.com>
2021-02-20 00:21:47 +0800

    delete unuse file

--- JetpackStudy/app/src/main/java/com/mahao/jetpackstudy/mvp/model/IGrilModel.java
+++
@@ -1,7 +0,0 @@
-package com.mahao.jetpackstudy.mvp.model;
-
-public interface IGrilModel {
-
-    public void loadGrilData();
-
-}
--- JetpackStudy/app/src/main/java/com/mahao/jetpackstudy/mvp/view/IBaseView.java
+++
@@ -1,5 +0,0 @@
-package com.mahao.jetpackstudy.mvp.view;
-
-public interface IBaseView {
-    void showErrorMessage(String msg);
-}
